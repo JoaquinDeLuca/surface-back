409e3099d087e60d0baed81dca566b06ac2a763c
Facu00H <faisher00@gmail.com>
2022-10-02 23:10:35 -0300

    create buzos model

---
+++ models/Buzos.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const buzosSchema = new mongoose.Schema({
+  jacketClosure: {type:Boolean, required: true},
+  description: {type:String, required:true},
+  photo: {type:String, required:true},
+  price: {type:Number, required:true},
+  stock: {type:Number, required:true},
+});
+
+const Buzos = mongoose.model('buzos', buzosSchema);
+module.exports = Buzos;
