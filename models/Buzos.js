6c21d263451d823dadc8fc43cd17c7c567c327c5
laza33salas <lazarocode@gmail.com>
2022-10-03 14:40:22 -0300

    add key name

--- models/Buzos.js
+++ models/Buzos.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
 const buzosSchema = new mongoose.Schema({
-  jacketClosure: {type:Boolean, required: true},
-  description: {type:String, required:true},
-  photo: {type:String, required:true},
-  price: {type:Number, required:true},
-  stock: {type:Number, required:true},
+  name: { type: String, required: true },
+  jacketClosure: { type: Boolean, required: true },
+  description: { type: String, required: true },
+  photo: { type: String, required: true },
+  price: { type: Number, required: true },
+  stock: { type: Number, required: true },
 });
 
 const Buzos = mongoose.model('buzos', buzosSchema);
--- models/Shirt.js
+++ models/Shirt.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
 const shirtSchema = new mongoose.Schema({
-    collarShirt: { type: Boolean, require: true},
-    description: {type:String, required:true},
-    photo: {type:String, required:true},
-    price: {type:Number, required:true},
-    stock: {type:Number, required:true},
+    name: { type: String, required: true },
+    collarShirt: { type: Boolean, require: true },
+    description: { type: String, required: true },
+    photo: { type: String, required: true },
+    price: { type: Number, required: true },
+    stock: { type: Number, required: true },
 })
 
-const shirt = mongoose.model('tshirts', shirtSchema);
-module.exports = shirt;
+const Shirt = mongoose.model('tshirts', shirtSchema);
+module.exports = Shirt;
