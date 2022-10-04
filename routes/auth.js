26b42d046b50d6b8a4b2b3d25e5541b9887a869d
adrieldussex <adrieldussex@gmail.com>
2022-10-04 17:31:53 -0300

    Add signIn route

--- routes/auth.js
+++ routes/auth.js
@@ -4,10 +4,12 @@ var router = express.Router();
 
 const {
     signUp,
+    signIn,
     verifyMail,
 } = require('../controllers/userController')
 
 router.post('/signup', signUp)
+router.post('/signin', signIn)
 router.post('/verification/:code', verifyMail)
 
 module.exports = router
