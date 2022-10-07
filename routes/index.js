806f446d91970fba009325d1d55e74aa070c17fb
FranRomero209 <francromero@protonmail.com>
2022-10-07 18:46:14 -0300

    route index -> concurse

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,19 @@
-var express = require('express');
-const shirt = require('./shirt');
-const cap = require('./cap');
-const buzo = require('./buzo');
-const user = require('./auth')
+var express = require("express");
+const shirt = require("./shirt");
+const cap = require("./cap");
+const buzo = require("./buzo");
+const user = require("./auth");
+const concurse = require("./concurse");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Surface' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Surface" });
 });
 
-router.use('/tshirts', shirt)
-router.use('/caps', cap)
-router.use('/buzos', buzo)
-router.use('/auth', user)
+router.use("/tshirts", shirt);
+router.use("/caps", cap);
+router.use("/buzos", buzo);
+router.use("/auth", user);
+router.use("/concurses", concurse);
 module.exports = router;
