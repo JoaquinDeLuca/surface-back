558193852e05fc1e3b3b265afbf5a0f6e5936996
FranRomero209 <francromero@protonmail.com>
2022-10-07 18:45:45 -0300

    modelo concurse

--- models/Concurse.js
+++ models/Concurse.js
@@ -5,6 +5,7 @@ const concurseSchema = new mongoose.Schema({
   description: { type: String, required: true },
   photo: { type: String, required: true },
   likes: { type: Array, required: true },
+  course: { type: Array, required: true },
 });
 
 const User = mongoose.model("concurses", userSchema);
